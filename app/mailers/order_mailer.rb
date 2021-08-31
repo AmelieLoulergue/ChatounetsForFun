2a8713836987eaa8caa4601cc4be6bfccfe719c2
Ecris ton nom ici, en gardant les quotes <Ecris ton email ici, en gardant les quotes>
2021-08-31 11:47:12 +0200

    email confirmation to admin OK

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,8 +1,13 @@
 class OrderMailer < ApplicationMailer
 
-  def order_confirmation_email(order)
+  def order_confirmation_email_to_client(order)
     @order = order
     mail(to: order.user.email, subject: "Petit chaton arrive...")
+  end
+
+  def order_confirmation_email_to_admin(order)
+    @order = order
+    mail(to: "jojo.justman@gmail.com", subject: "Un petit chaton de plus dans la nature...")
   end
 
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,13 +2,17 @@ class Order < ApplicationRecord
   has_many :order_items, foreign_key: 'order_id', dependent: :destroy
   has_many :items, through: :order_items
   belongs_to :user
-  after_update :send_order_confirmation_email
+  after_update :send_order_confirmation_emails
 
   # Possibilité d'enregistrer le prix total de la commande dans la table orders et mettre à jour la table avec le prix total, une fois que tous les order-items
   # ont été créés => cela donne l'occasion de faire un update qui déclenchera l'envoi d'un email
 
-  def send_order_confirmation_email
-    OrderMailer.order_confirmation_email(self).deliver_now
+  def send_order_confirmation_emails
+    # email de confirmation au client
+    OrderMailer.order_confirmation_email_to_client(self).deliver_now
+
+    # email de confirmation à l'admin
+    OrderMailer.order_confirmation_email_to_admin(self).deliver_now
   end
 
 end
