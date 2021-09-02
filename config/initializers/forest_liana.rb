38ef68fd2705f86ffad4f570600b6c946d3e0fda
AmelieLoulergue <loulergueamelie@yahoo.fr>
2021-09-02 12:00:13 +0200

    fix forest liana v2

--- config/initializers/forest_liana.rb
+++ config/initializers/forest_liana.rb
@@ -1,3 +1,3 @@
-ForestLiana.env_secret = Rails.application.secrets.forest_env_secret_staging
-ForestLiana.auth_secret = Rails.application.secrets.forest_auth_secret_staging
-ForestLiana.application_url = Rails.application.secrets.forest_application_url_staging
+ForestLiana.env_secret = Rails.application.secrets.forest_env_secret
+ForestLiana.auth_secret = Rails.application.secrets.forest_auth_secret
+ForestLiana.application_url = Rails.application.secrets.forest_application_url
